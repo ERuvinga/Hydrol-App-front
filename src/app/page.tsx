671b4623b978ebdef53c5e875a975847829d9985
ERuvinga <ruvingaelie@gmail.com>
2023-11-22 23:44:26 +0200

    animation Leading Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
+'use client' // use client side page
+
+import {MainLeadingPage, LoadingPage} from "@/Components/laedingPage";
+import { useEffect, useState } from "react";
+
 export default function Home() {
+
+  const [preloadingState, setPreloadingState] = useState(false);
+  useEffect(()=>{
+      // setTimeout(()=>{
+      //   setPreloadingState(false);
+      // },5000)
+  },[])
+
   return (
-    <section className="">
-      <h1>Hydrol-App</h1>
+    <section className="AppContainer">
+      { preloadingState?
+        <LoadingPage/>:
+        <MainLeadingPage/>
+      }
     </section>
   )
 }
