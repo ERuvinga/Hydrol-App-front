1c5f8a6ddcedcec37e9811d32a4298a416f9112e
ERuvinga <ruvingaelie@gmail.com>
2023-11-23 20:07:00 +0200

    adding Pretier and Eslint file config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,22 @@
 'use client' // use client side page
 
-import {MainLeadingPage, LoadingPage} from "@/Components/laedingPage";
-import { useEffect, useState } from "react";
+import { MainLeadingPage, LoadingPage } from '../Components/laedingPage';
+import { useEffect, useState } from 'react';
+
+var user = 'Elie';
 
 export default function Home() {
+    const [preloadingState, setPreloadingState] = useState(true);
+    useEffect(() => {
+        setTimeout(() => {
+            setPreloadingState(false);
+        }, 5000);
 
-  const [preloadingState, setPreloadingState] = useState(true);
-  useEffect(()=>{
-      setTimeout(()=>{
-        setPreloadingState(false);
-      },5000)
-  },[])
+    }, []);
 
-  return (
-    <section className="AppContainer">
-      { preloadingState?
-        <LoadingPage/>:
-        <MainLeadingPage/>
-      }
-    </section>
-  )
+    return (
+        <section className="AppContainer">
+            {preloadingState ? <LoadingPage /> : <MainLeadingPage />}
+        </section>
+    );
 }
--- src/components/laedingPage.tsx
+++ src/components/laedingPage.tsx
@@ -1,36 +1,47 @@
 // native Lib
-import Link from "next/link";
+import Link from 'next/link';
 
 // dev modules
-import style from "../Style/leading.module.css"
-import Loading from "@/Components/loading";
+import style from '../Style/leading.module.css';
+import Loading from '@/Components/loading';
 
-const LoadingPage = () =>{
-    return(
-            <section className={`${style.main}`}>
-                <Loading/>
-                <span className={`${style.nameApp}`}>Hydrol-App</span>
-            </section>
-    )
+const LoadingPage = () => {
+    return (
+        <section className={`${style.main}`}>
+            <Loading />
+            <span className={`${style.nameApp}`}>Hydrol-App</span>
+        </section>
+    );
 };
 
-const MainLeadingPage = ()=>{
-    return(
-        <section className={`${style.ContainerLeadingPage}` }>
-            <img alt="Manage Illustration" className={`${style.LeadingIllustrat}`} src={"/img/home.png"}/>
+const MainLeadingPage = () => {
+    return (
+        <section className={`${style.ContainerLeadingPage}`}>
+            <img
+                alt="Manage Illustration"
+                className={`${style.LeadingIllustrat}`}
+                src={'/img/home.png'}
+            />
             <div className={`${style.Description}`}>
-                <h1  className={`${style.HeadDescr}`}>Meilleure platforme de gestion d`eau</h1>
-                <p  className={`${style.paragDescr}`}>connectez-vous à votre compte pour gérer et visualiser toutes vos données personnelles ( Litres utilisés, factures, etc.).</p>
+                <h1 className={`${style.HeadDescr}`}>
+                    Meilleure platforme de gestion d`eau
+                </h1>
+                <p className={`${style.paragDescr}`}>
+                    connectez-vous à votre compte pour gérer et visualiser
+                    toutes vos données personnelles ( Litres utilisés, factures,
+                    etc.).
+                </p>
                 <div className={`${style.AllBtns}`}>
-                    <Link className={`${style.BtnHome}`} href={"/home"}>Suivant</Link>
-                    <Link className={`${style.BtnLogin}`} href={"/Login"}>Connexion</Link>
+                    <Link className={`${style.BtnHome}`} href={'/home'}>
+                        Suivant
+                    </Link>
+                    <Link className={`${style.BtnLogin}`} href={'/Login'}>
+                        Connexion
+                    </Link>
                 </div>
             </div>
         </section>
-    )
+    );
 };
 
-export {
-    LoadingPage,
-    MainLeadingPage
-}
+export { LoadingPage, MainLeadingPage };
