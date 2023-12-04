b78232794cc06dd783ebe860db789b4c444f76c6
ERuvinga <ruvingaelie@gmail.com>
2023-12-04 15:30:24 +0200

    adding logout button in Other Account

--- src/Components/commonComponents/NavAuthUser.tsx
+++ src/Components/commonComponents/NavAuthUser.tsx
@@ -1,7 +1,13 @@
 import style from '@/Style/leading.module.css';
-import { Cog6ToothIcon, HomeIcon } from '@heroicons/react/24/outline';
+import {
+    ArrowLeftOnRectangleIcon,
+    Cog6ToothIcon,
+    HomeIcon,
+} from '@heroicons/react/24/outline';
 import Image from 'next/image';
 import Link from 'next/link';
+import WrapperCompnent from './wrapperComponent';
+import { useState } from 'react';
 
 interface DatasOfMainUser {
     name: string;
@@ -11,36 +17,55 @@ interface DatasOfMainUser {
 }
 
 const AuthNav = (datas: DatasOfMainUser) => {
+    const [StateLogOut, setStateLogOut] = useState(false);
     return (
-        <div className={`${style.Nav}`}>
-            <div className={'profilDatas'}>
-                <Image
-                    alt={'profil'}
-                    src={
-                        datas.image != ''
-                            ? `/img/${datas.image}`
-                            : '/img/profile.png'
-                    }
-                    className="profilImg"
-                    width={100}
-                    height={100}
-                />
-                <div className="Identity">
-                    <span className="name">{datas.name}</span>
-                    <span className="tel">
-                        {datas.email == '' ? datas.Tel : datas.email}
-                    </span>
+        <>
+            <div className={`${style.Nav}`}>
+                <div className={'profilDatas'}>
+                    <Image
+                        alt={'profil'}
+                        src={
+                            datas.image != ''
+                                ? `/img/${datas.image}`
+                                : '/img/profile.png'
+                        }
+                        className="profilImg"
+                        width={100}
+                        height={100}
+                    />
+                    <div className="Identity">
+                        <span className="name">{datas.name}</span>
+                        <span className="tel">
+                            {datas.email == '' ? datas.Tel : datas.email}
+                        </span>
+                    </div>
+                </div>
+                <div className={style.loginBtn}>
+                    <Link href={'/AuthO/AllUser'}>
+                        <HomeIcon className={style.icones} />
+                    </Link>
+                    <Link href={'/AuthO/AllUser/Profil'}>
+                        <Cog6ToothIcon className={style.icones} />
+                    </Link>
+                    <Link
+                        href={'#'}
+                        className={style.LogoutBtn}
+                        onClick={() => setStateLogOut(true)}
+                    >
+                        <ArrowLeftOnRectangleIcon className={style.icones} />
+                    </Link>
                 </div>
             </div>
-            <div className={style.loginBtn}>
-                <Link href={'/AuthO/AllUser'}>
-                    <HomeIcon className={style.icones} />
-                </Link>
-                <Link href={'/AuthO/AllUser/Profil'}>
-                    <Cog6ToothIcon className={style.icones} />
-                </Link>
-            </div>
-        </div>
+            <>
+                {StateLogOut ? (
+                    <WrapperCompnent
+                        text={'Voulez vous quitté ?'}
+                        setStatePopup={setStateLogOut}
+                        Action={'logout'}
+                    />
+                ) : null}
+            </>
+        </>
     );
 };
 
