ce8fcdd70d6ff2862487a54926392dae106d33a7
ERuvinga <ruvingaelie@gmail.com>
2023-12-05 07:58:28 +0200

    adding Facture and function printing this

--- src/app/AuthO/Admin/Menages/page.tsx
+++ src/app/AuthO/Admin/Menages/page.tsx
@@ -13,24 +13,35 @@ import AdminNav from '@/Components/commonComponents/NavAuthAdmin';
 import Link from 'next/link';
 
 //Atoms
-import { AllUsers } from '@/States/Users';
-import { ArrowPathIcon, PlusIcon } from '@heroicons/react/24/outline';
+import { AllUsers, compteurDatas } from '@/States/Users';
+import {
+    ArrowPathIcon,
+    PlusIcon,
+    PrinterIcon,
+    XMarkIcon,
+} from '@heroicons/react/24/outline';
 import CardUsers from '@/Components/commonComponents/cardUser';
 import WrapperCompnent from '@/Components/commonComponents/wrapperComponent';
+import { nowDate } from '@/app/Lib/Date';
 
 const Index = () => {
-    // states and atoms
+    // states
+    const [MainPage, setMainPage] = useState(true);
     const [loadingPage, setLoadingPage] = useState(false);
     const [searchUsers, setSearchUsers] = useState(true);
     const [ReloadUsers, setReloadUsers] = useState(false);
     const [ConfirmDelete, setConfirmDelete] = useState(false);
+    const [userSelected, setUserSelected] = useState(0);
+    const litre_$ = 0.5;
+
+    //Atoms
     const [DatasOfAuthUser, setDatasOfAuthUser]: any = useRecoilState(AuthUser);
     const [AllUserByAdmin, setAllUserByAdmin]: any = useRecoilState(AllUsers);
+    const ValueOfCompter = useRecoilValue(compteurDatas);
     const Api_Url = useRecoilValue(Link_toApi);
 
     //initializ States
     const Router = useRouter();
-
     useEffect(() => {
         // check if token of user is valid
         withAuth(
@@ -69,66 +80,220 @@ const Index = () => {
         <>
             {loadingPage || DatasOfAuthUser ? (
                 <main className="AppContainer">
-                    <AdminNav
-                        name={DatasOfAuthUser.name}
-                        email={DatasOfAuthUser.email}
-                        Tel={DatasOfAuthUser.tel}
-                        image={DatasOfAuthUser.picture}
-                    />
-                    <section
-                        className="
+                    <div className="NavBarnoPrinting">
+                        <AdminNav
+                            name={DatasOfAuthUser.name}
+                            email={DatasOfAuthUser.email}
+                            Tel={DatasOfAuthUser.tel}
+                            image={DatasOfAuthUser.picture}
+                        />
+                    </div>
+                    <>
+                        {MainPage ? (
+                            <section
+                                className="
                     DisplayDatasOfUser"
-                    >
-                        <span className="RefreshDatas">
-                            <Link href="/AuthO/Admin/Menages/NewUser">
-                                <span className="PlusBtn">
-                                    <PlusIcon className="Icone" />
-                                </span>
-                            </Link>
-                            <span
-                                className="ReloadBtn"
-                                onClick={() => {
-                                    setReloadUsers(!ReloadUsers);
-                                }}
                             >
-                                <ArrowPathIcon className="Icone" />
-                            </span>
-                        </span>
-                        {searchUsers || !AllUserByAdmin ? (
-                            <div className="LoadingUsers">
-                                <Loading WhiteOrBlack={false} />
-                                <span className="loadingText">
-                                    Recherche d`Utilisateurs ...
-                                </span>
-                            </div>
-                        ) : (
-                            <div className="containerCards">
-                                {AllUserByAdmin.length ? (
-                                    AllUserByAdmin.map(
-                                        (value: any, index: any) => (
-                                            <CardUsers
-                                                name={value.name}
-                                                email={value.email}
-                                                tel={value.tel}
-                                                picture={value.picture}
-                                                date={value.registerDate}
-                                                key={index}
-                                                id_Card={index}
-                                                idUser={value._id}
-                                                SetConfirmDelete={
-                                                    setConfirmDelete
-                                                }
-                                            />
-                                        )
-                                    )
-                                ) : (
-                                    <span className="notfundUser">
-                                        O`oops! Aucun utilisateur Trouvé
+                                <span className="RefreshDatas">
+                                    <Link href="/AuthO/Admin/Menages/NewUser">
+                                        <span className="PlusBtn">
+                                            <PlusIcon className="Icone" />
+                                        </span>
+                                    </Link>
+                                    <span
+                                        className="ReloadBtn"
+                                        onClick={() => {
+                                            setReloadUsers(!ReloadUsers);
+                                        }}
+                                    >
+                                        <ArrowPathIcon className="Icone" />
                                     </span>
+                                </span>
+                                {searchUsers || !AllUserByAdmin ? (
+                                    <div className="LoadingUsers">
+                                        <Loading WhiteOrBlack={false} />
+                                        <span className="loadingText">
+                                            Recherche d`Utilisateurs ...
+                                        </span>
+                                    </div>
+                                ) : (
+                                    <div className="containerCards">
+                                        {AllUserByAdmin.length ? (
+                                            AllUserByAdmin.map(
+                                                (value: any, index: any) => (
+                                                    <CardUsers
+                                                        name={value.name}
+                                                        email={value.email}
+                                                        tel={value.tel}
+                                                        picture={value.picture}
+                                                        date={
+                                                            value.registerDate
+                                                        }
+                                                        key={index}
+                                                        id_Card={index}
+                                                        idUser={value._id}
+                                                        SetConfirmDelete={
+                                                            setConfirmDelete
+                                                        }
+                                                        SetMainPage={
+                                                            setMainPage
+                                                        }
+                                                        setUserSelected={
+                                                            setUserSelected
+                                                        }
+                                                    />
+                                                )
+                                            )
+                                        ) : (
+                                            <span className="notfundUser">
+                                                O`oops! Aucun utilisateur Trouvé
+                                            </span>
+                                        )}
+                                    </div>
                                 )}
-                            </div>
+                            </section>
+                        ) : (
+                            <section className="DisplayDatasOfUser">
+                                <span className="ReturnToMain">
+                                    <span
+                                        className="ReturnMainPageBtn"
+                                        onClick={() => {
+                                            setMainPage(!MainPage);
+                                        }}
+                                    >
+                                        <XMarkIcon className="Icone" />
+                                    </span>
+                                </span>
+                                <div className="SateOut">
+                                    <span className="title">
+                                        Facture de consommation d`eau
+                                    </span>
+                                    <div className="ContainerIdUser">
+                                        <div className="AdminUser">
+                                            <span className="idFacture">
+                                                ID_ Administrateur
+                                            </span>
+                                            <span>
+                                                Nom:{' '}
+                                                <span className="cible">
+                                                    {DatasOfAuthUser.name}
+                                                </span>
+                                            </span>
+                                            <span>
+                                                Tél:{' '}
+                                                <span className="cible">
+                                                    {DatasOfAuthUser.tel}
+                                                </span>
+                                            </span>
+                                            <span>
+                                                email:{' '}
+                                                <span className="cible">
+                                                    {DatasOfAuthUser.email}
+                                                </span>
+                                            </span>
+                                        </div>
+                                        <div className="SubComptr">
+                                            <span className="idFacture">
+                                                ID_ sous compteur
+                                            </span>
+                                            <span>
+                                                Nom:{' '}
+                                                <span className="cible">
+                                                    {
+                                                        AllUserByAdmin[
+                                                            userSelected
+                                                        ].name
+                                                    }
+                                                </span>
+                                            </span>
+                                            <span>
+                                                Tél:{' '}
+                                                <span className="cible">
+                                                    {
+                                                        AllUserByAdmin[
+                                                            userSelected
+                                                        ].tel
+                                                    }
+                                                </span>
+                                            </span>
+                                            <span>
+                                                email:{' '}
+                                                <span className="cible">
+                                                    {
+                                                        AllUserByAdmin[
+                                                            userSelected
+                                                        ].email
+                                                    }
+                                                </span>
+                                            </span>
+                                        </div>
+                                    </div>
+                                    <div className="containerDatasFacture">
+                                        <span className="noStateOut">
+                                            No Compteur:{' '}
+                                            <span className="cible">
+                                                000
+                                                {
+                                                    AllUserByAdmin[userSelected]
+                                                        .idCompteur
+                                                }
+                                            </span>
+                                        </span>
+                                        <span className="noStateOut">
+                                            No Facture:{' '}
+                                            <span className="cible">
+                                                000
+                                                {AllUserByAdmin[userSelected]
+                                                    .idCompteur - 1}
+                                            </span>
+                                        </span>
+                                        <span className="noStateOut">
+                                            Date:{' '}
+                                            <span className="cible">
+                                                {nowDate(Date.now())}
+                                            </span>
+                                        </span>
+                                    </div>
+                                    <div className="ContainerConsommation">
+                                        <span className="noStateOut">
+                                            Litres Consommés:{' '}
+                                            <span className="cible">
+                                                {
+                                                    ValueOfCompter[userSelected]
+                                                        .litre
+                                                }
+                                            </span>
+                                        </span>
+                                        <span className="noStateOut">
+                                            $/Litre:{' '}
+                                            <span className="cible">
+                                                {litre_$}$
+                                            </span>
+                                        </span>
+                                        <span className="noStateOut">
+                                            Total à payer:{' '}
+                                            <span className="cible">
+                                                {litre_$ *
+                                                    ValueOfCompter[userSelected]
+                                                        .litre}
+                                                $
+                                            </span>
+                                        </span>
+                                    </div>
+                                    <div className="containerprintBtn">
+                                        <button
+                                            className="printStateOutBtn"
+                                            onClick={() => {
+                                                window.print();
+                                            }}
+                                        >
+                                            <PrinterIcon className="Icone" />
+                                        </button>
+                                    </div>
+                                </div>
+                            </section>
                         )}
-                    </section>
+                    </>
                 </main>
             ) : (
                 <div className="LoaderPage">
